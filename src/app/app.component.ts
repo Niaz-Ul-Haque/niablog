8629d718299501994a84d0c32944af16db635e8e
Niaz Ul Haque <theniaz619.2@gmail.com>
2020-08-13 10:50:21 -0400

    v1.1 - Fixed Bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,3 @@
-/*********************************************************************************
-* WEB422 – Assignment 05
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
-* assignment has been copied manually or electronically from any other source (including web sites) or
-* distributed to other students.
-*
-* Name: Mohammed Niaz Ul Haque Student ID: 149257180 Date: 23/07/2020
-*
-********************************************************************************/ 
-
 import { Component } from '@angular/core';
 
 @Component({
